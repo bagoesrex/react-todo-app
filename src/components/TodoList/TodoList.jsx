5ddb674e41eeae7be9059f11ae2758108c46484f
bagoesrex <bagoesrex@gmail.com>
2025-06-20 08:24:36 +0700

    feat: show message when todo list is empty

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,6 +5,7 @@ export default function TodoList({ todos, onUpdate, onDelete }) {
     return (
         <section>
             <h3>To-Do's</h3>
+            {!todos.length && <p>Maaf, kamu tidak punya to-do's</p>}
 
             <ul className={styles.TodoList}>
                 {todos.map((todo) => (
