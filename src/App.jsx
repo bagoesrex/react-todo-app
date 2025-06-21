0e473bf3b87148930f58233ec3de7eded61156b5
bagoesrex <bagoesrex@gmail.com>
2025-06-22 03:27:27 +0700

    refactor: extract todos logic into useTodos custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,11 @@
-import { useEffect, useState } from 'react'
 import styles from './App.module.css'
 import TodoForm from './components/TodoForm/TodoForm'
 import TodoList from './components/TodoList/TodoList';
 import TodoFilters from './components/TodoFilters/TodoFilters';
-import { api } from './api';
+import { useTodos } from './components/hooks/todo';
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const [filters, setFilters] = useState({})
-
-  async function fetchTodos() {
-    try {
-      const data = await api.todos.getAll(filters)
-      setTodos(data)
-    } catch (error) {
-      console.log("Gagal fetch Todos")
-    }
-  }
-
-  useEffect(() => {
-    fetchTodos()
-  }, [filters])
-
-  async function handleCreate(newTodo) {
-    try {
-      await api.todos.create(newTodo)
-      await fetchTodos()
-    } catch (error) {
-      console.log("Gagal membuat Todo")
-    }
-  }
-
-  async function handleUpdate(id, newTodo) {
-    try {
-      await api.todos.update(id, newTodo)
-      await fetchTodos()
-    } catch (error) {
-      console.log("Gagal memperbarui Todo")
-    }
-  }
-
-  async function handleDelete(id) {
-    try {
-      await api.todos.delete(id)
-      await fetchTodos()
-    } catch (error) {
-      console.log("Gagal menghapus Todo")
-    }
-  }
+  const todos = useTodos()
 
   return (
     <div className={styles.App}>
@@ -57,9 +15,9 @@ function App() {
       </header>
 
       <div className={styles.AppContainer}>
-        <TodoForm onCreate={handleCreate} />
-        <TodoFilters onFilter={setFilters} />
-        <TodoList todos={todos} onUpdate={handleUpdate} onDelete={handleDelete} />
+        <TodoForm onCreate={todos.create} />
+        <TodoFilters onFilter={todos.filter} />
+        <TodoList todos={todos.data} onUpdate={todos.update} onDelete={todos.delete} />
       </div>
     </div>
   )
