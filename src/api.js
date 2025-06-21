7f24559bee8e02c1b12067a04a5441f8f2b72c17
bagoesrex <bagoesrex@gmail.com>
2025-06-22 02:16:15 +0700

    refactor: simplify API endpoints with BASE_URL constant

--- src/api.js
+++ src/api.js
@@ -1,22 +1,21 @@
+const BASE_URL = import.meta.env.VITE_MOCKAPI_BASE_URL;
+
 export const api = {
   todos: {
     getAll(params = {}) {
       const searchParams = new URLSearchParams(params).toString();
 
-      return fetch(
-        `${import.meta.env.VITE_MOCKAPI_BASE_URL}todos?${searchParams}`,
-        {
-          method: "GET",
-          headers: { "content-type": "application/json" },
-        }
-      ).then((res) => {
+      return fetch(`${BASE_URL}todos?${searchParams}`, {
+        method: "GET",
+        headers: { "content-type": "application/json" },
+      }).then((res) => {
         if (res.ok) return res.json();
         if (res.status === 404) return [];
       });
     },
 
     create(data) {
-      return fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos`, {
+      return fetch(`${BASE_URL}todos`, {
         method: "POST",
         headers: { "content-type": "application/json" },
         body: JSON.stringify(data),
@@ -26,7 +25,7 @@ export const api = {
     },
 
     update(id, data) {
-      return fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos/${id}`, {
+      return fetch(`${BASE_URL}todos/${id}`, {
         method: "PUT",
         headers: { "content-type": "application/json" },
         body: JSON.stringify(data),
@@ -36,7 +35,7 @@ export const api = {
     },
 
     delete(id) {
-      return fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos/${id}`, {
+      return fetch(`${BASE_URL}todos/${id}`, {
         method: "DELETE",
       }).then((res) => {
         if (res.ok) return res.json();
